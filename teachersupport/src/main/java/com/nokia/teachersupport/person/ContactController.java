5e3ec716a65a671b5e5383ddc8856aa0856d0625
Karolina Makowska <karolina.makowska.ext@nokia.com>
2018-08-02 15:39:45 +0200

    Contact Start

--- teachersupport/src/main/java/com/nokia/teachersupport/person/ContactController.java
+++ teachersupport/src/main/java/com/nokia/teachersupport/person/ContactController.java
@@ -1,6 +1,8 @@
 package com.nokia.teachersupport.person;
 
 import com.nokia.teachersupport.currentUser.CurrentUser;
+import com.nokia.teachersupport.personSecurity.IUserSecurityDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +13,20 @@ import java.util.Objects;
 @Controller
 public class ContactController {
 
+    private IPersonService personService;
+    private IUserSecurityDataService userSecurityDataService;
+
+    @Autowired
+    public ContactController (IPersonService personService,IUserSecurityDataService userSecurityDataService) {
+        this.personService = personService;
+        this.userSecurityDataService=userSecurityDataService;
+    }
+
     @GetMapping("/teacherSupportContact")
     String contact(Model model){
+        Person person=personService.getPersonByUserSecurityData(userSecurityDataService.getUserSecurityDataByEmail(CurrentUser.getCurrentUserName()));
         model.addAttribute("currentUserName",Objects.requireNonNull(CurrentUser.getCurrentUserName()));
+        model.addAttribute("currentUserPerson",person);
         return "teacherSupportContact";
     }
 }
