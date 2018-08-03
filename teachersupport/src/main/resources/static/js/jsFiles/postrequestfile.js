23733b928b7c874760cd7772a4e8cdfaec4f4387
Karolina Makowska <karolina.makowska.ext@nokia.com>
2018-08-03 11:31:21 +0200

    onClick zmiana konwencji

---
+++ teachersupport/src/main/resources/static/js/jsFiles/postrequestfile.js
@@ -0,0 +1,28 @@
+
+
+function doAjax(formName, listFiles, typ) {
+
+//    event.preventDefault();
+    formName='#'+formName;
+    listFiles='#'+listFiles;
+
+    var form = $(formName)[0];
+    var data = new FormData(form);
+
+    $.ajax({
+        type: "POST",
+        enctype: 'multipart/form-data',
+        url: '/upload/'+typ,
+        data: data,
+
+        processData: false,
+        contentType: false,
+        cache: false,
+        success: (data) => {
+        $(listFiles).text(data);
+},
+    error: (e) => {
+        $(listFiles).text(e.responseText);
+    }
+});
+}
