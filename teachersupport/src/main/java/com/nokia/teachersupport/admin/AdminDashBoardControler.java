bbd853ac4cc8afc5cf0ae2cfba27f5003646afee
Jakub <jawalendowski@gmail.com>
2018-08-06 10:52:35 +0200

    modified:   teachersupport/src/main/java/com/nokia/teachersupport/admin/AdminDashBoardControler.java 	modified:   teachersupport/src/main/java/com/nokia/teachersupport/fileUpload/UpdateFileController.java 	modified:   teachersupport/src/main/resources/static/js/jsFiles/teacherSupportAdminDashboardFiles.js 	modified:   teachersupport/src/main/resources/templates/teacherSupportAdminDashboard.html

--- teachersupport/src/main/java/com/nokia/teachersupport/admin/AdminDashBoardControler.java
+++ teachersupport/src/main/java/com/nokia/teachersupport/admin/AdminDashBoardControler.java
@@ -6,17 +6,20 @@ import com.nokia.teachersupport.faculty.Faculty;
 import com.nokia.teachersupport.person.Person;
 import com.nokia.teachersupport.personSecurity.UserSecurityData;
 import com.nokia.teachersupport.roles.SecutityRole;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 import java.util.Objects;
+
 
 @Controller
 public class AdminDashBoardControler {
@@ -98,5 +101,57 @@ public class AdminDashBoardControler {
         }
         return "redirect:/teacherSupportAdminDashboard";
     }
+    @PostMapping("/teacherSupportAdminDashboard/newUserAdminActionFromFile")
+    String addNewUserFromFile(@RequestParam("uploadfile") MultipartFile file)  throws IOException {
+        if(file.getOriginalFilename().equals(""))
+            return "FAIL! \n" +
+                    "You did not choose a file.";
+        File convFile = new File(file.getOriginalFilename());
+        convFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        FileReader fr = new FileReader(convFile);
+        BufferedReader bufferedReader = new BufferedReader(fr);
 
+        try {
+            String[] parts = bufferedReader.readLine().split(",");
+            do {
+                    String myName=parts[0];
+                    String mySurname=parts[1];
+                    String myFaculty=parts[2];
+                    String myRole=parts[3];
+                    String myEmail=parts[4];
+                if ((adminDashboardService.getUserSecurityDataByEmail(myEmail) == null)&&
+                        (adminDashboardService.getFacultyByName(myFaculty) !=null)&&
+                        (adminDashboardService.getRoleByName(myRole) !=null) ) {
+                    Person person = new Person();
+                    UserSecurityData userSecurityData = new UserSecurityData();
+                    Faculty faculty = adminDashboardService.getFacultyByName(myFaculty);
+                    SecutityRole secutityRole=adminDashboardService.getRoleByName(myRole);
+                    person.setNameField(myName);
+                    person.setSurnameField(mySurname);
+                    faculty.addPersonToFaculty(person);//!
+                    person.setFacultyField(faculty); //to nie wiem czy potrzebne bo sa zabezpieczenia w obie str
+                    userSecurityData.setActive(false);
+                    userSecurityData.setEmail(myEmail);
+                    userSecurityData.setPassword("NULL");
+                    userSecurityData.setMatchingPassword("NULL");
+                    userSecurityData.addARole(secutityRole);
+                    secutityRole.addUserSecurityDataToRole(userSecurityData);
+                    person.setUserSecurityDataField(userSecurityData);
+                    adminDashboardService.saveUserPersonDataAdminAction(person);
+                    adminDashboardService.saveUserSecurityDataAdminAction(userSecurityData);
+                    adminDashboardService.saveUserFacultyDataAdminAction(faculty);
+                    adminDashboardService.saveUserRoleDataAdminAction(secutityRole);
+                }
+                parts = bufferedReader.readLine().split(",");
+            } while (parts[0] != null);
+        } finally {
+            bufferedReader.close();
+        }
+
+
+        return "SUCCES";
+    }
 }
