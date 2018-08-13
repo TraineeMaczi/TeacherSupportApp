ac06034d1f4d960a5cbc6e630c058bed94688f18
Jakub <jawalendowski@gmail.com>
2018-08-13 14:06:29 +0200

    'addFacultyPictures'

--- teachersupport/src/main/resources/static/js/jsRadios/SupportIndex.js
+++ teachersupport/src/main/resources/static/js/jsRadios/SupportIndex.js
@@ -3,10 +3,58 @@ $(document).ready(
 
     function () {
 
-        document.getElementById('9').src="img/logo.jpg";
+        var photo=[];
+        var id=[];
+         $.ajax({
+            type: "GET",
+            url: '/index/giveMePhoto',
+            success: function (result) {
+                if (result.status == "success") {
+
+                    photo=result.data;
+
+                } else {
+                    photo="img/logo.jpg" ;
+                }
+                console.log(result);
+
+                $.ajax({
+                    type: "GET",
+                    url: '/index/giveMeId',
+                    success: function (result) {
+                        if (result.status == "success") {
+
+                            id=result.data;
+
+                        } else {
+                            id=result.data;
+                        }
+                        console.log(result);
+                        var i=0;
+                        while(id[i])
+                        {
+                            document.getElementById(id[i]).src=photo[i];
+                            i++;
+                        }
+                    },
+                    error: function (e) {
+                        alert("Error!")
+                        console.log("ERROR: ", e);
+                    }
+
+                });
+
+            },
+            error: function (e) {
+                photo="img/logo.jpg";
+                alert("Error!")
+                console.log("ERROR: ", e);
+            }
+
+        });
+
         $("#confirmFacultyButton").on('click', function () {
             var item = $('input[name=selectedFaculty]:checked', '#selectFaculty').val();
-            event.preventDefault();
 
             $.ajax({
                 type: "POST",
