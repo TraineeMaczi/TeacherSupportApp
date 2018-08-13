eeb6a5b996fbfcddbffb36a4c3c3c68314a3d96c
Karolina Makowska <karolina.makowska.ext@nokia.com>
2018-08-13 08:49:23 +0200

    Fix delete news SUCCES error

--- teachersupport/src/main/java/com/nokia/teachersupport/newsP/HomeController.java
+++ teachersupport/src/main/java/com/nokia/teachersupport/newsP/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
     @PostMapping("/tshome/delete")
     String deleteNews(@RequestParam("id") Integer id) {
         newsService.deleteNews(id);
-        return "SUCCES";
+        return "redirect:/teacherSupportHome";
     }
 
 }
