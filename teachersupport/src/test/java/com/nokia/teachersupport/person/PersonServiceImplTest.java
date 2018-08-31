36148b1940750e7e367775086c157e2411030569
Karolina Makowska <karolina.makowska.ext@nokia.com>
2018-08-31 08:42:11 +0200

    Marge

--- teachersupport/src/test/java/com/nokia/teachersupport/person/PersonServiceImplTest.java
+++ teachersupport/src/test/java/com/nokia/teachersupport/person/PersonServiceImplTest.java
@@ -21,58 +21,58 @@ import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PersonServiceImplTest {
-//    Person person1 = new Person();
-//    Person person2 = new Person();
-//    Person person3 = new Person();
-//    List<Person> persons = new ArrayList<>();
-//    List<Person> admins = new ArrayList<>();
-//    UserSecurityData userSecurityData = new UserSecurityData();
-//    SecutityRole secutityRole = new SecutityRole();
-//    List<SecutityRole> secutityRoles = new ArrayList<>();
-//    @InjectMocks
-//    UserSecurityDataServiceImpl userSecurityDataService;
-//    @Mock
-//    PersonServiceImpl personService;
-//    @Mock
-//    PersonRepo personRepo;
-////    public void deleteAllPersons(IUserSecurityDataService userSecurityDataService) {
-////        boolean toDelete;
-////        for (Person person : personRepo.findAll()) {
-////            toDelete = true;
-////            for (SecutityRole securityRole : person.getUserSecurityDataField().getMyRoles())
-////                if (securityRole.getRoleName().equals("ADMIN"))
-////                    toDelete = false;
-////            if (toDelete)
-////                deletePerson(person,userSecurityDataService);
-////        }
-////    }
-//    @Before
-//    public void SetUp() {
-//        secutityRole.setRoleName("ADMIN");
-//        secutityRoles.add(secutityRole);
-//        userSecurityData.setMyRoles(secutityRoles);
-//        person1.setUserSecurityDataField(userSecurityData);
-//        persons.add(person1);
-//        admins.add(person1);
-//        secutityRole.setRoleName("USER");
-//        secutityRoles.clear();
-//        secutityRoles.add(secutityRole);
-//        userSecurityData.setMyRoles(secutityRoles);
-//        person2.setUserSecurityDataField(userSecurityData);
-//        persons.add(person2);
-//        userSecurityData.setMyRoles(secutityRoles);
-//        person3.setUserSecurityDataField(userSecurityData);
-//        persons.add(person3);
-//        when(personRepo.findAll()).thenReturn(persons);
-//        for (Person person : persons) {
-//            when(personService.deletePerson(person, userSecurityDataService)).thenReturn(persons.remove(person));
+    Person person1 = new Person();
+    Person person2 = new Person();
+    Person person3 = new Person();
+    List<Person> persons = new ArrayList<>();
+    List<Person> admins = new ArrayList<>();
+    UserSecurityData userSecurityData = new UserSecurityData();
+    SecutityRole secutityRole = new SecutityRole();
+    List<SecutityRole> secutityRoles = new ArrayList<>();
+    @InjectMocks
+    UserSecurityDataServiceImpl userSecurityDataService;
+    @Mock
+    PersonServiceImpl personService;
+    @Mock
+    PersonRepo personRepo;
+//    public void deleteAllPersons(IUserSecurityDataService userSecurityDataService) {
+//        boolean toDelete;
+//        for (Person person : personRepo.findAll()) {
+//            toDelete = true;
+//            for (SecutityRole securityRole : person.getUserSecurityDataField().getMyRoles())
+//                if (securityRole.getRoleName().equals("ADMIN"))
+//                    toDelete = false;
+//            if (toDelete)
+//                deletePerson(person,userSecurityDataService);
 //        }
 //    }
-//
-//    @Test
-//    public void deleteAllPersons() {
-//        personService.deleteAllPersons(userSecurityDataService);
-//        assertEquals(admins, persons);
-//
-//    }
+    @Before
+    public void SetUp() {
+        secutityRole.setRoleName("ADMIN");
+        secutityRoles.add(secutityRole);
+        userSecurityData.setMyRoles(secutityRoles);
+        person1.setUserSecurityDataField(userSecurityData);
+        persons.add(person1);
+        admins.add(person1);
+        secutityRole.setRoleName("USER");
+        secutityRoles.clear();
+        secutityRoles.add(secutityRole);
+        userSecurityData.setMyRoles(secutityRoles);
+        person2.setUserSecurityDataField(userSecurityData);
+        persons.add(person2);
+        userSecurityData.setMyRoles(secutityRoles);
+        person3.setUserSecurityDataField(userSecurityData);
+        persons.add(person3);
+        when(personRepo.findAll()).thenReturn(persons);
+        for (Person person : persons) {
+            when(personService.deletePerson(person, userSecurityDataService)).thenReturn(persons.remove(person));
+        }
+    }
+
+    @Test
+    public void deleteAllPersons() {
+        personService.deleteAllPersons(userSecurityDataService);
+        assertEquals(admins, persons);
+
+    }
 }
--- teachersupport/src/test/java/com/nokia/teachersupport/publications/PublicationsServiceImplTest.java
+++ teachersupport/src/test/java/com/nokia/teachersupport/publications/PublicationsServiceImplTest.java
@@ -1,37 +1,50 @@
-//package com.nokia.teachersupport.publications;
+package com.nokia.teachersupport.publications;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PublicationsServiceImplTest {
+    EditPublicationDTO editPublicationDTO=new EditPublicationDTO();
+    Publications oldPuli=new Publications();
+    Publications newPubli=new Publications();
+
+    @InjectMocks
+    PublicationsServiceImpl publicationsService;
+
+    @Mock
+    PublicationsRepo publicationsRepo;
+    @Before
+    public void SetUp()
+    {
+        newPubli.setPublicationsInfoField("New Info");
+        oldPuli.setPublicationsInfoField("Old Info");
+        editPublicationDTO.setNewContent("New Info");
+        editPublicationDTO.setOldContent("Old Info");
+        when(publicationsRepo.findByPublicationsInfoField(editPublicationDTO.getOldContent())).thenReturn(oldPuli);
+        }
+    @Test
+    public void goEditPublications() {
+        assertEquals(newPubli.getPublicationsInfoField(),publicationsService.goEditPublications(editPublicationDTO,).getPublicationsInfoField()); //ed publi
+    }
+}
+
 //
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//
-//import static org.junit.Assert.*;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class PublicationsServiceImplTest {
-//    EditPublicationDTO editPublicationDTO=new EditPublicationDTO();
-//    Publications oldPuli=new Publications();
-//    Publications newPubli=new Publications();
-//
-//    @InjectMocks
-//    PublicationsServiceImpl publicationsService;
-//
-//    @Mock
-//    PublicationsRepo publicationsRepo;
-//    @Before
-//    public void SetUp()
-//    {
-//        newPubli.setPublicationsInfoField("New Info");
-//        oldPuli.setPublicationsInfoField("Old Info");
-//        editPublicationDTO.setNewContent("New Info");
-//        editPublicationDTO.setOldContent("Old Info");
-//        when(publicationsRepo.findByPublicationsInfoField(editPublicationDTO.getOldContent())).thenReturn(oldPuli);
+//    @Override
+//    public Publications goEditPublications(EditPublicationDTO editPublicationDTO,IPersonService personService,IUserSecurityDataService userSecurityDataService) {
+//        Publications publication = new Publications();
+//        Person person = personService.getPersonByUserSecurityData(userSecurityDataService.getUserSecurityDataByEmail(CurrentUser.getCurrentUserName()));
+//        if (person.doIHaveAPublicationWithContent(editPublicationDTO.getOldContent()) != null && !editPublicationDTO.getNewContent().equals("")) {
+//            publication=person.doIHaveAPublicationWithContent(editPublicationDTO.getOldContent());
+//            publication.setPublicationsInfoField(editPublicationDTO.getNewContent());
+//            publicationsRepo.save(publication);
 //        }
-//    @Test
-//    public void goEditPublications() {
-//        assertEquals(newPubli.getPublicationsInfoField(),publicationsService.goEditPublications(editPublicationDTO,).getPublicationsInfoField()); //ed publi
+//        return publication;
 //    }
-//}
